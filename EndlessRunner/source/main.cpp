d341ea316f14202d4583b8616e81d42013e1e2a4
unknown <dje3873@GDD-49.main.ad.rit.edu>
2015-09-13 22:09:30 -0400

    added cube to game

--- EndlessRunner/source/main.cpp
+++ EndlessRunner/source/main.cpp
@@ -21,8 +21,10 @@ public:
 private:
 	ICamera3D*  m_camera3D;
 	IModel*     m_floor;
+	IModel*     m_player;
 	IFont*      m_font;
 	GameObject* m_go;
+	GameObject* m_po;
 	Transform   fontTransform;
 };
 
@@ -37,9 +39,11 @@ void EndlessRunner::VOnStartup()
 	m_renderer->VSetClearColor(Vixen::Colors::Black);
 
 	m_camera3D = m_renderer->Camera3D();
+	m_camera3D->VSetView(Vector3(0.0f, 5.0f,  -5.0f), Vector3(0.0f, 5.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f));
 	m_camera3D->VSetSpeed(50.0f);
 
 	m_floor = ResourceManager::OpenModel(VTEXT("floor.mdl"));
+	m_player = ResourceManager::OpenModel(VTEXT("cube.mdl"));
 	m_font = ResourceManager::OpenFont(VTEXT("Consolas_24.fnt"));
 
 
@@ -48,6 +52,10 @@ void EndlessRunner::VOnStartup()
 		0.0f, 0.0f, 0.0f,
 		1.0f, 1.0f, 1.0f));
 	m_go->SetModel(m_floor);
+
+	m_po = new GameObject(Transform(0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f));
+	m_po->SetModel(m_player);
+
 	fontTransform = Transform(20.0f, 20.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
 	
 
@@ -56,41 +64,43 @@ void EndlessRunner::VOnStartup()
 
 void EndlessRunner::VOnUpdate(float dt)
 {
-	if (m_keyboard->SingleKeyPress(IKEY::F2))
+	if (Input::SingleKeyPress(IKEY::F2))
 		m_window->VClose();
 
-	if (m_keyboard->KeyPress(IKEY::S))
+	if (Input::KeyPress(IKEY::S))
 		m_camera3D->VWalk(-dt);
 
-	if (m_keyboard->KeyPress(IKEY::W))
+	if (Input::KeyPress(IKEY::W))
 		m_camera3D->VWalk(dt);
 
-	if (m_keyboard->KeyPress(IKEY::A))
+	if (Input::KeyPress(IKEY::A))
 		m_camera3D->VStrafe(-dt);
 
-	if (m_keyboard->KeyPress(IKEY::D))
+	if (Input::KeyPress(IKEY::D))
 		m_camera3D->VStrafe(dt);
 
-	if (m_keyboard->KeyPress(IKEY::UP))
+	if (Input::KeyPress(IKEY::UP))
 	{
 		m_go->GetTransform()->TranslateY(dt);
 	}
 
 
-	int deltaX = m_mouse->DeltaX(m_window->VGetClientBounds().w / 2);
+	/*int deltaX = m_mouse->DeltaX(m_window->VGetClientBounds().w / 2);
 	int deltaY = m_mouse->DeltaY(m_window->VGetClientBounds().h / 2);
 	m_camera3D->VRotateX(deltaY * 0.25f);
 	m_camera3D->VRotateY(deltaX * 0.25f);
 
+	m_camera3D->VUpdate(dt);*/
 	m_camera3D->VUpdate(dt);
 
-	m_window->VTrapCursorCenter();
+	/*m_window->VTrapCursorCenter();*/
 }
 
 void EndlessRunner::VOnRender(float dt)
 {
 	
 	m_go->Render(m_camera3D);
+	m_po->Render(m_camera3D);
 
 	//ALL 2D UI IS DRAW AFTER SCENE IS DRAWN
 	USStream ss;
