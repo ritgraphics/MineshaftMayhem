e7a2936d808a2a326e7428e943f6472f1229dd82
David Erbelding <dje3873@g.rit.edu>
2015-10-10 14:49:23 -0400

    more updates to shaders, added a slight twisting effect

--- EndlessRunner/assets/Shaders/DirectX/Model_VS.hlsl
+++ EndlessRunner/assets/Shaders/DirectX/Model_VS.hlsl
@@ -24,27 +24,50 @@ struct VertexToPixel
 
 VertexToPixel main(VertexShaderInput input, uint instanceID : SV_InstanceID)
 {
+
 	VertexToPixel output;
 
 	matrix world = transforms[instanceID];
 
 	output.position = mul(float4(input.position, 1.0f), world);
 
-	output.depth = output.position.z / output.position.w;
+	float depth = output.position.z / output.position.w;
 
-	float curvex = (sin((time*.0157 + output.depth)*.022) + cos((time*.0157 + output.depth)*.055)) * 2.0;
-	float curvey = (cos((time*.0157 + output.depth)*.033) + -sin((time*.0157 + output.depth)*.077));
-	/*matrix depthDistortion = { cos(output.depth*.1), -sin(output.depth*.1), 0.0, 0.0,
-	sin(output.depth * .1), cos(output.depth*.1), 0.0, 0.0,
-	0.0, 0.0, 1.0, 0.0,
-	0.0, 0.0, 0.0, 1.0 };*/
+	float t = time * .0157 + depth;
 
-	matrix depthDistortion = { 1.0, 0.0, 0.0, 0.0,
+	float curvex = (sin(t*.01) + cos(t*.05) - cos(t*.03))*2;
+	float curvey = cos(t*.07) - sin(t*.11) - sin(t*.02);
+
+	matrix depthDistortion = {
+		1.0, 0.0, 0.0, 0.0,
 		0.0, 1.0, 0.0, 0.0,
 		0.0, 0.0, 1.0, 0.0,
 		curvex, curvey, 0.0, 1.0 };
 
-	/*matrix depthDistortion = { 1.0, 0.0, 0.0, 0.0,
+	matrix depthDistortion2 = {
+		cos(-curvex*.05), -sin(-curvex*.05), 0.0, 0.0,
+		sin(-curvex*.05), cos(-curvex*.05), 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0 };
+
+	/*matrix depthDistortion = {
+		cos(depth*curvex*.002), 0.0, -sin(depth*curvex*.002), 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		sin(depth*curvex*.002), 0.0, cos(depth*curvex*.002), 0.0,
+		0.0, 0.0, 0.0, 1.0 };*/
+
+	depthDistortion = mul(depthDistortion, depthDistortion2);
+	
+	/*matrix curve = {
+		cos((t + output.depth) * .01), 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, cos((t + output.depth) * .01), 0.0,
+		0.0, 0.0, 0.0, 1.0 };*/
+
+	//depthDistortion = mul(depthDistortion, curve);
+
+	/*matrix depthDistortion = {
+	1.0, 0.0, 0.0, 0.0,
 	0.0, 1.0, 0.0, 0.0,
 	0.0, 0.0, 1.0, 0.0,
 	0.0, 0.0, 0.0, 1.0 };*/
@@ -55,6 +78,7 @@ VertexToPixel main(VertexShaderInput input, uint instanceID : SV_InstanceID)
 
 	output.normal = mul(input.normal, (float3x3)mul(world, depthDistortion));
 	output.uv = input.uv;
+	output.depth = depth;
 	output.time = time;
 
 	return output;
