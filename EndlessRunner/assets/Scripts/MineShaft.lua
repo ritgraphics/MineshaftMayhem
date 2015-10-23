22afa8a26dbf7bb35139a1705dc0ea79dd3a3cbb
David Erbelding <dje3873@g.rit.edu>
2015-10-22 21:44:20 -0400

    added more script logic for spawning rails, added speed variable that increases with time.

--- EndlessRunner/assets/Scripts/MineShaft.lua
+++ EndlessRunner/assets/Scripts/MineShaft.lua
@@ -1,15 +1,24 @@
 function MineShaft.OnInit()
 	this.railPrefab = Prefab.Load("rail.pfb");
 	this.railPrefab:MarkStore();
-	this.speed = 5.0;
-	this.position = 0.0;
-	this.test = 0.0;
+    this.maxDist = 120.0; --furthest rail position
+    this.moveSpeed = 10.0;
+	this.loopPosition = 0.0;
+    this.sectionLength = 6.0;
 	this.track = {};
 end
 
 function MineShaft.OnEnable()
+    MineShaft.SpawnStartingTrack();
 end
 
+function MineShaft.SpawnStartingTrack()
+    for i = 0, 20, 1 do
+        this.loopPosition = -1 * i * this.sectionLength;
+        MineShaft.MakeSegment();
+    end
+    this.loopPosition = 0.0;
+end
 
 function MineShaft.MakeSegment()
 	local row = {};
@@ -21,15 +30,22 @@ end
 
 function SpawnRail(column, prefab)
 	local rail = prefab:CreateObject();
-	rail:GetTransform().Position = Vector3(5.0 * column, 0.0, this.test);
+	rail:GetTransform().Position = Vector3(5.0 * column, 0.0, this.maxDist + this.loopPosition);
+    
+    local id = rail:GetID();
+    local scriptName = "Rail";
+
+    Rail.hash[""..id].manager = this;
+    
+
 	return rail;
 end
 
 function MineShaft.Update(dt)
-	this.position = this.position + (dt*this.speed);
-	this.test = this.test + (dt*this.speed);
-	if (this.position > 6.0) then
-		this.position = this.position - 6.0;
+    this.moveSpeed = this.moveSpeed + dt * 0.1;
+	this.loopPosition = this.loopPosition + (dt*this.moveSpeed);
+	if (this.loopPosition > this.sectionLength) then
+		this.loopPosition = this.loopPosition - this.sectionLength;
 		MineShaft.MakeSegment();
 	end
 	
